18401efd5cf334ac377b09cd644f02673a263b2b
sanket6460 <145358951+sanket6460@users.noreply.github.com>
2025-08-16 18:10:26 +0530

    Update and rename day 2 . java to day 2.java

--- day 2.java
+++ day 2.java
@@ -18,11 +18,7 @@ public class MissingNumber {
 
 
 
-
-
-
-
- # # Problem : Find the Missing Number
+Problem : Find the Missing Number
 You are given an array arr containing n-1 distinct integers. The array consists of integers taken from the range 1 to n, meaning one integer is missing from this sequence. Your task is to find the missing integer.
 
 Input :
